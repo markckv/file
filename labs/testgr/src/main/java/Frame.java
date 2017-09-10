9bb0db3bf2030383d98d6dad7fef9911fb025a72
markckv <markckv@gmail.com>
2017-09-10 22:34:48 +0700

    Add files via upload

--- labs/testgr/src/main/java/Frame.java
+++ labs/testgr/src/main/java/Frame.java
@@ -5,18 +5,21 @@ import main.java.cg.MainFrame;
 
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.PortUnreachableException;
 
 /**
  * Created by mark on 13.08.17.
  */
 public class Frame extends MainFrame {
+    File file;
     JPanell jp;
     public Frame(int i, int i1, String test) throws NoSuchMethodException {
         super(i,i1,test);
-        this.addSubMenu("testt",0);
-        this.addMenuItem("testt/test","test2",0,"p.png","f");
-        this.addToolBarButton("testt/test");
+        this.addSubMenu("File",0);
+        this.addMenuItem("File/open","open",0,"f.png","open");
+        this.addToolBarButton("File/open");
+
         jp = new JPanell();
         jp.setSize(this.getWidth(), this.getHeight());
         this.add(jp);
@@ -27,7 +30,22 @@ public class Frame extends MainFrame {
 
     }
 
-    public void f(){
-        System.out.print("4");
-    }
-}
+
+    public void open() throws IOException {file =  this.getOpenFileName("txt", "text");
+        Reader reader = new FileReader(file);
+        BufferedReader Breader = new BufferedReader(reader);
+        String string = Breader.readLine();
+        System.out.print(string);
+        string = Breader.readLine();
+        System.out.println(string);
+        Writer writer = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        bufferedWriter.write(string,0,string.length());
+        bufferedWriter.close();
+        Breader.close();
+        writer.close();
+        reader.close();
+
+
+
+}}
--- labs/testgr/src/main/java/cg/JPanell.java
+++ labs/testgr/src/main/java/cg/JPanell.java
@@ -8,99 +8,62 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class JPanell extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         BufferedImage BI = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
         Graphics graphics1 = BI.getGraphics();
+        graphics1.setColor(Color.WHITE);
+        graphics1.fillRect(0, 0, BI.getWidth(), BI.getHeight());
         graphics1.setColor(Color.RED);
-        graphics1.drawOval(50, 50, 50, 50);
-        graphics1.drawOval(60, 60, 10, 10);
-        fill(BI);
-        graphics.drawImage(BI, 50, 50, null);
 
+
+        BufferedImage BI2 = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics graphics12 = BI.getGraphics();
+       //graphics1.drawRect(50, 50, 50, 50);
+      //  graphics1.drawOval(50, 50, 50, 50);
+       // graphics1.drawOval(60, 60, 10, 10);
+        fill(graphics1);
+        graphics.drawImage(BI, 0, 0, null);
+//graphics.drawImage(BI2, 150,50,null);
     }
 
-    public void fill(BufferedImage BI) {
-        Span span = null;
-        Span span2 = null;
-        Span span3 = null;
-        ArrayList<Span> spans = new ArrayList<Span>();
-        int x, y;
-        x = 75;
-        y = 75;
-        int tx = 0;
-        int ty = 0;
-        int color = BI.getRGB(75, 75);
-       span =  paint(BI,y,x, color);
-        if(span != null) {
-            spans.add(span);
-span3 = span;
+    public void fill(Graphics graphics1 ) {
+        //!!!!!!!!!!!!!!!!!!!! Тут числа
+        int size = 10;
+        int size2 = 10;
+        int sizedraw = (size/2)+1;
+        int x=100;
+        int y=100;
+        for(int iy = 0; iy<size2; iy++){
+            y = 100+(60*iy);
+        for(int xi = 1; xi<sizedraw; xi++) {
+            draw(x+(xi*60), y, graphics1);
+            draw(x+30+(xi*60), y+30, graphics1);
         }
 
-        while (0 != spans.size()){
-            span = spans.get(spans.size()-1);
-            //System.out.println(span.xl+"| |"+span.xr+"| |"+span.y+"| |"+(span.xr-span.xl));
-            for(int c= 0; c< spans.size(); c++) {
-                span = spans.get(c);
-                for (int i = span.xl; i < span.xr; i++)
-                    BI.setRGB(i, span.y, Color.black.getRGB());
-            }
-            spans.remove(spans.size()-1);
-            for(int i = span.xl; i < span.xr; i++) {
-                span2 = paint(BI, span.y - 1, i, color);
-                if (span.y == 71) {
-                    System.out.println("Span" + span2.xl + " " + span2.xr);
-                    System.out.println("I = " + i);
-                }
-                if(span2 != null) {
-                    i = span.xr + 1;
-                    spans.add(span2);
-                }
-            }
-        }
-        span =  span3;
-        spans.add(span);
+        if(size % 2 != 0){
+            draw(x+(sizedraw*60), y, graphics1);
+        }}
 
-        while (0 != spans.size()){
-            span = spans.get(spans.size()-1);
-            //System.out.println(span.xl+"| |"+span.xr+"| |"+span.y);
-            spans.remove(spans.size()-1);
-            for(int i = span.xl ; i<span.xr; i++)
-                BI.setRGB(i, span.y, Color.black.getRGB());
-            for(int i = span.xl; i < span.xr; i++) {
-                span2 = paint(BI, span.y + 1, i, color);
-                if(span2 != null) {
-                    i = span.xr +1;
-                    spans.add(span2);
-                }
-            }
-        }
-
-
-
-
-    }
-    public Span paint(BufferedImage BI, int y, int x , int color){
-        Span span = new Span();
-        for (int i = 0; i < x; i++) {
-            if (BI.getRGB(x - i, y) != color) {
-                span = new Span();
-                span.xl = x - i+1;
-                span.y = y;
-                break;
-            }
-        }
-        for (int i = 1; i < x; i++) {
-            if (BI.getRGB(x + i, y) != color) {
-                span.xr = x + i;
-              return (span);
-            }
-
-
-        }
-        return (null);
-    }
 }
+
+public void draw(int x, int y, Graphics graphics1){
+        graphics1.drawLine(x,y,x+15,y+30);
+    graphics1.drawLine(x,y,x+15,y-30);
+
+    graphics1.drawLine(x+15,y+30,x+30,y+30);
+    graphics1.drawLine(x+15,y-30,x+30,y-30);
+
+    graphics1.drawLine(x+30,y+30,x+45,y);
+    graphics1.drawLine(x+30,y-30,x+45,y);
+}
+}
+
+
+
+
